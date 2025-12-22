136526b25b1db9978bf328396108eea08a6b7a96
htetwai18 <htetwai.18.lwin@gmail.com>
2025-12-22 18:09:57 +0700

    resume update

--- lib/pages/hero_section.dart
+++ lib/pages/hero_section.dart
@@ -991,7 +991,7 @@ class _HeroSectionState extends State<HeroSection>
   // }
 
   Future<void> _downloadResume() async {
-    final Uri uri = Uri.parse('https://drive.google.com/file/d/1YHUTzEtqxcwILWsvZhVN8lYZmwU3gAYE/view?usp=sharing');
+    final Uri uri = Uri.parse('https://drive.google.com/file/d/1BJt7LL-Qt4YgSnedOmzWu-OhozmDbg8a/view?usp=sharing');
     if (!await launchUrl(
       uri,
       mode: LaunchMode.externalApplication,
